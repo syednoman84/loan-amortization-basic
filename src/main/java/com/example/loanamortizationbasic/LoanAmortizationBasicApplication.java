f41723e9fd72e739ed97db9dac375d688cf70261
Noman Ahmed <noman.ahmed@linearft.com>
2022-09-14 05:34:51 -0400

    Boilerplate code

---
+++ src/main/java/com/example/loanamortizationbasic/LoanAmortizationBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.loanamortizationbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanAmortizationBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanAmortizationBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loanamortizationbasic/LoanAmortizationBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loanamortizationbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanAmortizationBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
